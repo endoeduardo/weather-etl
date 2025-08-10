1668f1586d8016fb1e3d0502cd90285318c7060f
endoeduardo <eduardo.t.endo@gmail.com>
2025-08-10 16:58:40 -0300

    FEAT: add admin user to weather_db

--- mongo-init/init.js
+++ mongo-init/init.js
@@ -1,5 +1,17 @@
-// Sample MongoDB initialization script
-// Replace with your actual schema and data
-
+// Switch to your target database
 db = db.getSiblingDB('weather_db');
+
+// Create a collection (optional)
 db.createCollection('weather_data');
+
+// Create a user for this database
+db.createUser({
+  user: "admin",
+  pwd: "password", // change this
+  roles: [
+    {
+      role: "readWrite",
+      db: "weather_db"
+    }
+  ]
+});
